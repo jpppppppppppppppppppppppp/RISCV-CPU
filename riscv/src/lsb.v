ed9f91c69d94d31aa877773877b98ba9cd24eb5f
jpppppppppppppppppppppppp <1490881945@qq.com>
2023-12-13 00:23:14 +0800

    2023/12/13 周三  0:23

---
+++ riscv/src/lsb.v
@@ -0,0 +1,9 @@
+module lsb(
+    input   wire            clk,
+    input   wire            rst,
+    input   wire            rdy,
+
+    input   wire            rollback_config,
+);
+    
+endmodule //lsb
